2ebc6f8f231b6ce1da0c4a49d5d405e16d08f7e1
Zhi Chen <cz.michael@gmail.com>
2015-10-25 21:24:52 -0400

    add gulp live reload, separate out the components of SPA into different html files

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var gulp   = require('gulp'),
     less = require('gulp-less'),
     livereload = require('gulp-livereload');
 
+
+livereload({ start: true });
+
+
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
 
@@ -17,30 +21,42 @@ var HEALTHFULL_U_DEV_LOCATION = 'src/main/webapp/static';
 gulp.task('jshint', function() {
 	return gulp.src(HEALTHFULL_U_DEV_LOCATION + '/*')
     	.pipe(jshint())
-    	.pipe(jshint.reporter('jshint-stylish'));
+    	.pipe(jshint.reporter('jshint-stylish'))
+    	.pipe(livereload());
 });
 
 gulp.task('scripts-healthfull_u', function() {
 	return gulp.src(HEALTHFULL_U_DEV_LOCATION + '/*')
 		.pipe(gulp.dest(HEALTHFULL_U_TOMCAT_LOCATION + '/js/healthfull_u'))
+		.pipe(livereload())
 		.pipe(notify('======== scripts-healthfull_u is done done. =========='));
 });
 
 gulp.task('scripts-healthfull_u-controllers', function() {
 	return gulp.src(HEALTHFULL_U_DEV_LOCATION + '/js/healthfull_u/controllers/*')
 		.pipe(gulp.dest(HEALTHFULL_U_TOMCAT_LOCATION + '/js/healthfull_u/controllers'))
+		.pipe(livereload())
 		.pipe(notify('======== scripts-healthfull_u-controllers done. =========='));
 });
 
 gulp.task('scripts-healthfull_u-services', function() {
 	return gulp.src(HEALTHFULL_U_DEV_LOCATION + '/js/healthfull_u/services/*')
 		.pipe(gulp.dest(HEALTHFULL_U_TOMCAT_LOCATION + '/js/healthfull_u/services'))
+		.pipe(livereload())
 		.pipe(notify('======== scripts-healthfull_u-services done. =========='));
+});
+
+gulp.task('admin-lte', function() {
+	return gulp.src(HEALTHFULL_U_DEV_LOCATION + '/plugins/AdminLTE/js/*')
+		.pipe(gulp.dest(HEALTHFULL_U_TOMCAT_LOCATION + '/plugins/AdminLTE/js'))
+		.pipe(livereload())
+		.pipe(notify('======== admin-lte done. =========='));
 });
 
 gulp.task('copy-html', function() {
 	return gulp.src(HEALTHFULL_U_DEV_LOCATION + '/*.html')
 		.pipe(gulp.dest(HEALTHFULL_U_TOMCAT_LOCATION + '/'))
+		.pipe(livereload())
 		.pipe(notify('======== copy-html done. =========='));
 });
 
@@ -48,6 +64,7 @@ gulp.task('less', function () {
 	return gulp.src(HEALTHFULL_U_DEV_LOCATION + '/less/healthfull_u/healthfullU.less')
 		.pipe(less())
 		.pipe(gulp.dest(HEALTHFULL_U_TOMCAT_LOCATION + '/css/healthfull_u'))
+		.pipe(livereload())
 		.pipe(notify('======== less done. =========='));;
 });
 
@@ -60,8 +77,12 @@ gulp.task('watch', function() {
 	gulp.watch(HEALTHFULL_U_DEV_LOCATION + '/*.js', ['jshint', 'scripts-healthfull_u']);
 	gulp.watch(HEALTHFULL_U_DEV_LOCATION + '/controllers/*.js', ['jshint', 'scripts-healthfull_u-controllers']);
 	gulp.watch(HEALTHFULL_U_DEV_LOCATION + '/services/*.js', ['jshint', 'scripts-healthfull_u-services']);
+	gulp.watch(HEALTHFULL_U_DEV_LOCATION + '/plugins/AdminLTE/js/*.js', ['jshint', 'admin-lte']);
 	gulp.watch(HEALTHFULL_U_DEV_LOCATION + '/*.html', ['copy-html']);
 	gulp.watch(HEALTHFULL_U_DEV_LOCATION + '/less/**/*.less', ['less']);
 });
+
+
+
 
 
